8cd1d95d2d67d37e9b12403865c97a38de62384d
User <User@WINDOWS-M75NR9M>
2021-08-06 22:25:04 +0800

    spring

---
+++ spring/src/main/java/com/study/spring/case01/MainTest2.java
@@ -0,0 +1,22 @@
+package com.study.spring.case01;
+
+/*
+public class husband {
+	private String name;
+	private Wife wife;
+	//...
+}
+
+public class Wife {
+	private String name;
+	private Husband husband;
+	//....
+}
+用 Spring 配置
+John 的老婆是 Mary
+Mary 的丈夫是 John
+*/
+
+public class MainTest2 {
+
+}
