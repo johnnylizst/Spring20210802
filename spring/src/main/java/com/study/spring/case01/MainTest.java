02a440ffc2ebe4c934a042bdf869e02bb625dee8
User <User@WINDOWS-M75NR9M>
2021-08-06 20:46:27 +0800

    spring

--- spring/src/main/java/com/study/spring/case01/MainTest.java
+++ spring/src/main/java/com/study/spring/case01/MainTest.java
@@ -1,10 +1,21 @@
 package com.study.spring.case01;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//Author author = ctx.getBean(Author.class);
+		Author author1 = (Author)ctx.getBean("author1");
+		Author author2 = (Author)ctx.getBean("author2");
+		Author author3 = (Author)ctx.getBean("author3");
+		Author author4 = (Author)ctx.getBean("author4");
+		System.out.println(author1);
+		System.out.println(author2);
+		System.out.println(author3);
+		System.out.println(author4);
 	}
 
 }
