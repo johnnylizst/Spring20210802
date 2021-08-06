a46cbbf0d84473a8b8146d43306045efe6b87ba0
User <User@WINDOWS-M75NR9M>
2021-08-06 21:12:58 +0800

    spring

--- spring/src/main/java/com/study/spring/case01/MainTest.java
+++ spring/src/main/java/com/study/spring/case01/MainTest.java
@@ -16,6 +16,14 @@ public class MainTest {
 		System.out.println(author2);
 		System.out.println(author3);
 		System.out.println(author4);
+		Book book1 = (Book)ctx.getBean("book1");
+		Book book2 = (Book)ctx.getBean("book2");
+		Book book3 = (Book)ctx.getBean("book3");
+		Book book4 = (Book)ctx.getBean("book4");
+		System.out.println(book1);
+		System.out.println(book2);
+		System.out.println(book3);
+		System.out.println(book4);
 	}
 
 }
