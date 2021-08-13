ef55f023fb5a312383ee9ed3c0e02b98f0b3f01d
User <User@WINDOWS-M75NR9M>
2021-08-13 20:29:46 +0800

    spring

---
+++ spring/src/main/java/com/study/spring/case03/UserTest.java
@@ -0,0 +1,14 @@
+package com.study.spring.case03;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext3.xml");
+		
+
+	}
+
+}
---
+++ spring/src/main/java/com/study/spring/case03/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.study.spring.case03.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+	
+	public UserController() {
+		System.out.println("UserController()");
+	}
+	
+}
